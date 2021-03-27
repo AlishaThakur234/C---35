af1fefdb9245743048c799a81b60ee8679a0a67c
Alisha.Thakur <umeshkumar.thakur@tges.org>
2021-03-27 16:57:42 +0530

    Car Racing - Class 1

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 var ball;
+var dataBase
+var pos
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
+    ball = createSprite(250,250,30,30);
     ball.shapeColor = "red";
+
+    dataBase = firebase.database ();
+
+    var posDb = dataBase.ref ("position");
+
+    posDb.on("value",readPosition)
 }
 
 function draw(){
-    background("white");
+    background("lightgrey");
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -24,6 +32,23 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    // ball.x = ball.x + x;
+    // ball.y = ball.y + y;
+
+    dataBase.ref ("position").set({
+        x: pos.x + x,
+    
+        y:pos.y + y
+    })
+
+}
+
+
+function readPosition (x){
+//x is the trigger
+pos = x.val()
+
+ball.x = pos.x
+ball.y = pos.y
+
 }
